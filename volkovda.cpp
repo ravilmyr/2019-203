d3b4af300ca7a603d683228821bb5f8d8d052452
Волков Дмитрий Алексеевич <volkovda@lab227.local>
2019-02-25 08:02:25 +0300

    lab5 fix

--- volkovda.cpp
+++ volkovda.cpp
@@ -1,5 +1,8 @@
 ﻿#include "volkovda.h"
-#define EPSILON 1e-10
+
+constexpr double EPSILON = 1.e-15;
+
+
 
 /**
  * Введение в дисциплину
@@ -10,47 +13,40 @@ void volkovda::lab1()
 }
 
 
+
 /**
  * Метод Гаусса с выбором главного элемента
  */
-
-// Меняет местами строки СЛАУ
-void row_swap(double **A, double *b, int N, int i, int j)
-{
-	double tmp;
-
-	tmp = b[i];
-	b[i] = b[j];
-	b[j] = tmp;
-
-	for (int k = 0; k < N; k++)
-	{
-		tmp = A[i][k];
-		A[i][k] = A[j][k];
-		A[j][k] = tmp;
-	}
-}
-
 void volkovda::lab2()
 {
+	// Отслеживаем позиции вектора x
+	int *p = new int[N];
+	for (int i = 0; i < N; i++) {
+		p[i] = i;
+	}
+
 	for (int k = 0; k < N; k++) {
 
 		// Выбор ведущего элемента
 		int d = k;
 		for (int i = k + 1; i < N; i++) {
-			if (abs(A[d][k]) < abs(A[i][k])) {
+			if (std::abs(A[d][k]) < std::abs(A[i][k])) {
 				d = i;
 			}
 		}
 
 		// Меняем строки
 		if (d != k) {
-			row_swap(A, b, N, d, k);
+			for (int j = k; j < N; j++) {
+				std::swap(A[d][j], A[k][j]);
+			}
+			std::swap(p[d], p[k]);
+			std::swap(b[d], b[k]);
 		}
 
 		// Система не определена
-		if (A[k][k] < EPSILON) {
-			std::cout << "System of linear equations has no solution.";
+		if (std::abs(A[k][k]) < EPSILON) {
+			std::cout << "System of linear equations has no solution.\n";
 			return;
 		}
 
@@ -71,10 +67,12 @@ void volkovda::lab2()
 		}
 	}
 
-	// Записываем результат в вектор x
+	// Записываем результат в вектор x с учетом измененных позиций
 	for (int i = 0; i < N; i++) {
-		x[i] = b[i];
+		x[i] = b[p[i]];
 	}
+
+	delete[] p;
 }
 
 
@@ -84,7 +82,25 @@ void volkovda::lab2()
  */
 void volkovda::lab3()
 {
+	double *v = new double[N];
+	double *w = new double[N];
 
+	// Прямой ход
+	v[0] = -A[0][1] / A[0][0];
+	w[0] = b[0] / A[0][0];
+	for (int i = 1; i < N; i++) {
+		v[i] = -A[i][i + 1] / (A[i][i] + A[i][i - 1] * v[i - 1]);
+		w[i] = (b[i] - A[i][i - 1] * w[i - 1]) / (A[i][i] + A[i][i - 1] * v[i - 1]);
+	}
+
+	// Обратный ход
+	x[N - 1] = w[N - 1];
+	for (int i = N - 2; i >= 0; i--) {
+		x[i] = v[i] * x[i + 1] + w[i];
+	}
+
+	delete[] v;
+	delete[] w;
 }
 
 
@@ -94,7 +110,54 @@ void volkovda::lab3()
  */
 void volkovda::lab4()
 {
+	// Вектор значений x на предыдущий итерации
+	double *px = new double[N];
+	for (int i = 0; i < N; i++) {
+		px[i] = 0.0;
+	}
 
+	/*int iteration = 0;*/
+	while (true) {
+		/*iteration++;*/
+
+		// Посчитаем значения неизвестных на текущей итерации
+		for (int i = 0; i < N; i++) {
+			x[i] = b[i];
+
+			// Исключаем i -ю неизвестную
+			// Вычислим сумму
+			for (int j = 0; j < N; j++) {
+				if (i != j) {
+					x[i] -= (A[i][j] * px[j]);
+				}
+			}
+
+			// Делим на коэффициент при i -ой неизвестной
+			x[i] /= A[i][i];
+		}
+
+		// Посчитаем максимальную по модулю погрешность
+		double error = 0.0;
+		for (int i = 0; i < N; i++) {
+			if (std::abs(x[i] - px[i]) > error) {
+				error = std::abs(x[i] - px[i]);
+			}
+		}
+
+		// При достижении необходимой точности завершаем процесс
+		if (error < EPSILON) {
+			break;
+		}
+
+		// Текущее зачение итерации представим как предыдущее
+		for (int i = 0; i < N; i++) {
+			px[i] = x[i];
+		}
+	}
+
+	/*std::cout << "Number of iterations : " << iteration << '\n';*/
+
+	delete[] px;
 }
 
 
@@ -104,7 +167,57 @@ void volkovda::lab4()
  */
 void volkovda::lab5()
 {
+	// Метод Зейделя (модификация метода простых итераций)
 
+	// Вектор значений x на предыдущий итерации
+	double *px = new double[N];
+	for (int i = 0; i < N; i++) {
+		px[i] = 0.0;
+	}
+
+	/*int iteration = 0;*/
+	while (true) {
+		/*iteration++;*/
+
+		// Посчитаем значения неизвестных на текущей итерации
+		for (int i = 0; i < N; i++) {
+			double var = 0.0;
+
+			// Исключаем i -ю неизвестную
+			// Вычисляем сумму k+1 -го шага
+			for (int j = 0; j < i; j++) {
+				var += (A[i][j] * x[j]);
+			}
+			// Вычисялем сумму k -го шага
+			for (int j = i + 1; j < N; j++) {
+				var += (A[i][j] * px[j]);
+			}
+
+			x[i] = (b[i] - var) / A[i][i];
+		}
+
+		// Посчитаем максимальную по модулю погрешность
+		double error = 0.0;
+		for (int i = 0; i < N; i++) {
+			if (std::abs(x[i] - px[i]) > error) {
+				error = std::abs(x[i] - px[i]);
+			}
+		}
+
+		// При достижении необходимой точности завершаем процесс
+		if (error < EPSILON) {
+			break;
+		}
+
+		// Текущее зачение итерации представим как предыдущее
+		for (int i = 0; i < N; i++) {
+			px[i] = x[i];
+		}
+	}
+
+	/*std::cout << "Number of iterations : " << iteration << '\n';*/
+
+	delete[] px;
 }
 
 
