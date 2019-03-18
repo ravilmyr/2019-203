3db367f4a78c5ecf6deb48fdf211316d3cef57b7
ravilmyr <47311916+ravilmyr@users.noreply.github.com>
2019-03-18 08:49:48 +0300

    Update murtazinrr.cpp

--- murtazinrr.cpp
+++ murtazinrr.cpp
@@ -14,7 +14,37 @@ cout <<"Hello world" << endl;
  */
 void murtazinrr::lab2()
 {
+  двойной p;
+	int maxn;
 
+    для (int k= 0; k<n-1; k++)
+    {
+ maxn = k;
+        for (int i=k+ 1; i)
+			если (abs (a[i][k]) > > abs (a[maxn][k])) maxn = i; 
+        std:: swap (a[maxn], A[k]); 
+        std:: swap (b[maxn], b[k]);
+
+        for (int i=k+ 1; i)
+        {
+ p = a[i][k] / a[k][k];
+            for (int j=k; j)
+ A[i] [j] - = p*A[k][j];
+ b[i] - = p*b[k];
+        }
+    }
+
+    for (int i = 0; i)
+    {
+ x [i]=b[i];
+    }
+
+    для (int i=N-1; i> = > 0 ; i--)
+    {
+        for (int j=i+ 1; j)
+ x[i]-=a[i] [j]*x[j];
+ x [i] / = a[i][i];
+    }
 }
 
 
