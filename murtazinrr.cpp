2d439c4e1c39faa1965a31e509acc59051f5214a
ravilmyr <47311916+ravilmyr@users.noreply.github.com>
2019-04-29 09:12:25 +0300

    Update murtazinrr.cpp

--- murtazinrr.cpp
+++ murtazinrr.cpp
@@ -14,6 +14,39 @@ cout <<"Hello world" << endl;
  */
 void murtazinrr::lab2()
 {
+двойной р;
+    int maxn;
+
+    для ( int k = 0 ; k <N- 1 ; k ++)
+    {
+        maxn = k;
+        для ( int i = k + 1 ; i <N; i ++)
+            if ( abs (A [i] [k])> abs (A [maxn] [k])) maxn = i;
+        std :: swap (A [maxn], A [k]);
+        std :: swap (b [maxn], b [k]);
+
+        для ( int i = k + 1 ; i <N; i ++)
+        {
+            p = A [i] [k] / A [k] [k];
+            для ( int j = k; j <N; j ++)
+                A [i] [j] - = p * A [k] [j];
+            b [i] - = p * b [k];
+        }
+    }
+
+    для ( int i = 0 ; i <N; i ++)
+    {
+        х [I] = Ь [I];
+    }
+
+    для ( int i = N- 1 ; i> = 0 ; i--)
+    {
+        для ( int j = i + 1 ; j <N; j ++)
+            х [г] - = А [I] [J] * х [J];
+        x [i] / = A [i] [i];
+    }
+}
+
 
 }
 
