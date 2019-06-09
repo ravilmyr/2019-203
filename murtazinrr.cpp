4772fa164a549c50722e8202bfd9973d51e69591
ravilmyr <47311916+ravilmyr@users.noreply.github.com>
2019-06-09 10:22:20 +0300

    Update murtazinrr.cpp

--- murtazinrr.cpp
+++ murtazinrr.cpp
@@ -181,7 +181,64 @@ delete [] oldx;
  */
 void murtazinrr::lab6()
 {
+double Eps = 1e-18;
+	double Del, Res, Abs;
 
+	double *K = new double[N];
+	double *L = new double[N];
+	double *xrez = new double[N];
+	
+	
+	
+	for (int i = 0; i<N; i++)
+		xrez[i] = 0;
+
+	
+	do{
+		
+		for (int i = 0; i < N; i++) {
+			K[i] = 0;
+			for (int j = 0; j < N; j++)
+				K[i] += A[i][j] * xrez[j];
+		}
+
+		
+		for (int i = 0; i < N; i++) {
+			L[i] = K[i] - b[i];
+		}
+
+		
+		for (int i = 0; i < N; i++) {
+			K[i] = 0;
+			for (int j = 0; j < N; j++)
+				K[i] += A[i][j] * L[j];
+		}
+
+		Res = 0;
+		Abs = 0;
+		
+	
+		for (int i = 0; i < N; i++) {
+			Res += K[i] * L[i];
+			Abs += K[i] * K[i];
+		}
+		
+		if (Res==Abs) Res=1;
+		else {
+		Res = Res / Abs;
+		}
+		
+		for (int i = 0; i < N; i++)
+			x[i] = xrez[i] - Res*L[i];
+		
+		
+		Del = abs(x[0] - xrez[0]);
+		for (int i = 0; i < N; i++) {
+			if (abs(x[i] - xrez[i])>Del)
+				Del = abs(x[i] - xrez[i]);
+			xrez[i] = x[i];
+		}
+	} while (Eps < Del);
 }
 
 
@@ -191,19 +248,157 @@ void murtazinrr::lab6()
  */
 void murtazinrr::lab7()
 {
+double Eps = 1e-20;
+	double Del, s, sAbs;
 
+
+	double *K = new double[N];
+	double *L = new double[N];
+	double *M = new double[N];
+	double *xrez = new double[N];
+	
+	
+	
+	for (int i = 0; i<N; i++){
+		xrez[i] = 0;
+	}
+	
+	
+	do {
+		
+		for (int i = 0; i < N; i++) {
+			K[i] = 0;
+			for (int j = 0; j < N; j++)
+				K[i] += A[i][j] * xrez[j];
+		}
+		
+		
+		for (int i = 0; i < N; i++) {
+			L[i] = K[i] - b[i];
+		}
+		
+		
+		for (int i = 0; i < N; i++) {
+			K[i] = 0;
+			for (int j = 0; j < N; j++)
+				K[i] += A[i][j] * L[j];
+		}
+		
+		
+		for (int i = 0; i < N; i++) {
+			M[i] = 0;
+			for (int j = 0; j < N; j++) {
+				M[i] += A[i][j] * K[j];
+			}
+		}
+		
+		s = 0;
+		sAbs = 0;
+		
+		
+		for (int i = 0; i < N; i++) {
+			s += K[i] * L[i];
+			sAbs += M[i] * K[i];
+		}
+		if (s == sAbs)
+			s = 1;
+		else 
+			s = s / sAbs;
+		
+		for (int i = 0; i < N; i++)
+			x[i] = xrez[i] - s*L[i];
+		
+		
+		Del = abs(x[0] - xrez[0]);
+		
+		for (int i = 0; i < N; i++) {
+			if (abs(x[i] - xrez[i])>Del)
+				Del = abs(x[i] - xrez[i]);
+				xrez[i] = x[i];
+		}
+	} while (Eps < Del);
 }
 
 
 void murtazinrr::lab8()
 {
+double **H = new double*[N], eps = 1.e-10;
+    for (int i = 0; i < N; i++) H[i] = new double[N];
 
+    do
+    {
+        double n = 0;
+        int i_max = 0, j_max = 1;
+        for (int i = 0; i < N; i++)
+            for (int j = i + 1; j < N; j++)
+            {
+                if (fabs(A[i][j]) > fabs(A[i_max][j_max]))
+                {
+                    i_max = i;
+                    j_max = j;
+                }
+
+                n += A[i][j] * A[i][j];
+            }
+
+        if (sqrt(n) < eps) break;
+
+        double fi = 0.5 * atan(2 * A[i_max][j_max] / (A[i_max][i_max] - A[j_max][j_max]));
+        for (int i = 0; i < N; i++)
+        {
+            H[i][i_max] = A[i][i_max] * cos(fi) + A[i][j_max] * sin(fi);
+            H[i][j_max] = A[i][j_max] * cos(fi) - A[i][i_max] * sin(fi);
+        }
+
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                if (j != i_max && j != j_max) H[i][j] = A[i][j];
+
+        for (int j = 0; j < N; j++)
+        {
+            A[i_max][j] = H[i_max][j] * cos(fi) + H[j_max][j] * sin(fi);
+            A[j_max][j] = H[j_max][j] * cos(fi) - H[i_max][j] * sin(fi);
+        }
+
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                if (i != i_max && i != j_max) A[i][j] = H[i][j];
+
+    }while(true);
+
+    for (int i = 0; i < N; i++) x[i] = A[i][i];
+
+    for (int i = 0; i < N; i++) delete[] H[i];
+    delete[] H;
 }
 
 
 void murtazinrr::lab9()
 {
-
+double eps=pow(10,-10), lambda=0, newlambda=0, y[N], mody;
+    x[0]=1;
+    do{
+        for (int i=0; i<N; i++){
+            y[i]=0;
+            for (int j=0; j<N; j++){
+                y[i]+=A[i][j]*x[j];
+            }
+        }
+        lambda=newlambda;
+        newlambda=0;
+        for (int i=0; i<N; i++){
+            newlambda+=y[i]*x[i];
+        }
+        mody=0;
+        for (int i=0; i<N; i++){
+            mody+=pow(y[i], 2);
+        }
+        mody=sqrt(mody);
+        for (int i=0; i<N; i++){
+            x[i]=y[i]/mody;
+        }
+    }while (abs(lambda-newlambda)>eps);
+    x[0]=newlambda;
 }
 
 
